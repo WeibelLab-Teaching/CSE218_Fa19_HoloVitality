775d9dce7ce93f61a7a0ba179d23ea273d6df29b
KeSun <sk461236626@163.com>
2019-11-24 16:48:07 -0800

    added data transmit

--- New Unity Project/Assets/Scripts/contentControl.cs
+++ New Unity Project/Assets/Scripts/contentControl.cs
@@ -18,12 +18,13 @@ public class contentControl : MonoBehaviour
     private int criticalRate=121;
     private int nextUpdate=1;
 
+
     private TcpClient client;
     private NetworkStream nwStream;
 
-    const int PORT_NO = 5000;
-    const string SERVER_IP = "127.0.0.1";
-    const int BUFFERSIZE = 24;
+    const int PORT_NO = 12346;
+    const string SERVER_IP = "100.80.226.240";
+    const int BUFFERSIZE = 8;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,50 +32,50 @@ public class contentControl : MonoBehaviour
         textM = GetComponent<TextMeshPro>();
         textM.text = "HR: --- /min";
 
-        //client = new TcpClient(SERVER_IP, PORT_NO);
-        //nwStream = client.GetStream();
+        client = new TcpClient(SERVER_IP, PORT_NO);
+        nwStream = client.GetStream();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Time.time>=nextUpdate){
-            heartRate = 90+4*nextUpdate%40;
+            //heartRate = 90+4*nextUpdate%40;
 
-            if (heartRate >= criticalRate)
-            {
-                textM.color = new Color32(255, 0, 0, 255);
-            }
-            else
-            {
-                textM.color = new Color32(255, 255, 255, 255);
-            }
-            textM.text = "HR: " + heartRate.ToString() + "/min";
-
-
-            //byte[] bytesToRead = new byte[BUFFERSIZE];
-            //int numRead = nwStream.Read(bytesToRead, 0, 24);
-
-            //if (numRead == BUFFERSIZE)
+            //if (heartRate >= criticalRate)
             //{
-            //    heartRate = (int)BitConverter.ToDouble(bytesToRead, 0);
-
-            //    variance = (int)BitConverter.ToDouble(bytesToRead, 8);
-
-            //    timeStamp = BitConverter.ToInt64(bytesToRead, 16);
-
-            //    if (heartRate >= criticalRate)
-            //    {
-            //        textM.color = new Color32(255, 0, 0, 255);
-            //    }
-            //    else
-            //    {
-            //        textM.color = new Color32(255, 255, 255, 255);
-            //    }
-            //    textM.text = "HR: " + heartRate.ToString() + "/min";
+            //    textM.color = new Color32(255, 0, 0, 255);
             //}
+            //else
+            //{
+            //    textM.color = new Color32(255, 255, 255, 255);
+            //}
+            //textM.text = "HR: " + heartRate.ToString() + "/min";
 
-            nextUpdate+=1;
+
+            byte[] bytesToRead = new byte[BUFFERSIZE];
+            int numRead = nwStream.Read(bytesToRead, 0, 8);
+
+            if (numRead == BUFFERSIZE)
+            {
+                heartRate = (int)BitConverter.ToDouble(bytesToRead, 0);
+
+                //variance = (int)BitConverter.ToDouble(bytesToRead, 8);
+
+                //timeStamp = BitConverter.ToInt64(bytesToRead, 16);
+
+                if (heartRate >= criticalRate)
+                {
+                    textM.color = new Color32(255, 0, 0, 255);
+                }
+                else
+                {
+                    textM.color = new Color32(255, 255, 255, 255);
+                }
+                textM.text = "HR: " + heartRate.ToString() + "/min";
+            }
+
+            nextUpdate +=1;
         }
         
     }
--- New Unity Project/Assets/Scripts/contentControl2.cs
+++ New Unity Project/Assets/Scripts/contentControl2.cs
@@ -1,37 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using TMPro;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System;
+
 
 public class contentControl2 : MonoBehaviour
 {
     // Start is called before the first frame update
     private TextMeshPro textM;
-    private int RespiratoryRate;
-    private int criticalRate=25;
+    private int variance;
+    private int criticalRate=10;
     private int nextUpdate=1;
+
+    private TcpClient client;
+    private NetworkStream nwStream;
+
+    const int PORT_NO = 12347;
+    const string SERVER_IP = "100.80.226.240";
+    const int BUFFERSIZE = 8;
     // Start is called before the first frame update
     void Start()
     {
-        RespiratoryRate = 12;
+        variance = 0;
         textM = GetComponent<TextMeshPro>();
         textM.color = Color.cyan;
         textM.text = "RR: -- /min";
+
+        client = new TcpClient(SERVER_IP, PORT_NO);
+        nwStream = client.GetStream();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Time.time>=nextUpdate){
-            RespiratoryRate = 12+3*nextUpdate%20;
-            if (RespiratoryRate>=criticalRate){
-                textM.color = Color.yellow;
-            }else{
-                textM.color = Color.cyan;
-            }
-            textM.text = "RR: "+RespiratoryRate.ToString()+"/min";
+            //RespiratoryRate = 12+3*nextUpdate%20;
+            //if (RespiratoryRate>=criticalRate){
+            //    textM.color = Color.yellow;
+            //}else{
+            //    textM.color = Color.cyan;
+            //}
+            //textM.text = "RR: "+RespiratoryRate.ToString()+"/min";
 
-            nextUpdate+=1;
+            //nextUpdate+=1;
+
+
+            byte[] bytesToRead = new byte[BUFFERSIZE];
+            int numRead = nwStream.Read(bytesToRead, 0, 8);
+
+            if (numRead == BUFFERSIZE)
+            {
+                //heartRate = (int)BitConverter.ToDouble(bytesToRead, 0);
+
+                variance = (int)BitConverter.ToDouble(bytesToRead, 0);
+
+                //timeStamp = BitConverter.ToInt64(bytesToRead, 16);
+
+                if (variance >= criticalRate)
+                {
+                    textM.color = Color.yellow;
+                }
+                else
+                {
+                    textM.color = Color.cyan;
+                }
+                textM.text = "RR: " + variance.ToString() + "/min";
+            }
+
+            nextUpdate += 1;
         }
         
     }
